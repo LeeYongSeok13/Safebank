7304c67fe2f9078453167bb1d540946eac521cb9
LeeYongSeok13 <minamiharuka333@gmail.com>
2026-05-29 18:14:08 +0900

    finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
 $(function () {
   // html 문서를 모두 로딩 후 스크립트를 읽겠다.
+
+  // 헤더
+  $("nav>ul>li").mouseover(function () {
+    $(".sub, .menuBg").stop().slideDown();
+  });
+  $("nav>ul>li").mouseout(function () {
+    $(".sub, .menuBg").stop().slideUp();
+  });
+
+  // 슬라이드
+  let i = 0;
+  function slide() {
+    if (i < 2) {
+      i++;
+    } else {
+      i = 0;
+    }
+    $(".slide ul li").fadeOut();
+    $(".slide ul li").eq(i).fadeIn();
+  }
+
+  setInterval(slide, 3000);
+
+  // 탭메뉴
+  let t;
+
+  $(".tabmenu >li").click(function () {
+    $(".tabmenu>li").removeClass("on");
+    $(this).addClass("on");
+
+    t = $(this).index();
+    console.log(t);
+
+    $(".tabcon").hide();
+    $(".tabcon").eq(t).show();
+    return false;
+  });
+
+  // 팝업
+  $(".pop").click(function () {
+    $(".popup").show();
+  });
+  $(".close button").click(function () {
+    $(".popup").hide();
+  });
 });
